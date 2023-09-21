b321145d4afd2550619dcfcf244a35de987d4025
yangxr <yangxr1995@gmail.com>
2023-09-21 05:48:06 -0400

    iptables

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 set -e
 
 topdir=${PWD}
@@ -6,18 +6,21 @@ patchdir=${topdir}/patch
 imagedir=${topdir}/images
 rootfsdir=${topdir}/rootfs
 toolsdir=${topdir}/tools
+builddir=${topdir}/build
 
 busybox_src="busybox-1.32.0"
 linux_src="linux-5.10.183"
 uboot_src="u-boot-2020.10"
 gdb_src="gdb-13.2"
 qemu_src="qemu-2.8.0"
+iptables_src="iptables-1.8.9"
 
 busybox_tar="${busybox_src}.tar.bz2"
 linux_tar="${linux_src}.tar.xz"
 uboot_tar="${uboot_src}.tar.bz2"
 gdb_tar="${gdb_src}.tar.xz"
 qemu_tar="${qemu_src}.tar.xz"
+iptables_tar="${iptables_src}.tar.xz"
 
 declare -A src_arr
 src_arr["linux"]="${topdir}/build/${linux_src}"
@@ -25,6 +28,7 @@ src_arr["busybox"]="${topdir}/build/${busybox_src}"
 src_arr["uboot"]="${topdir}/build/${uboot_src}"
 src_arr["gdb"]="${topdir}/build/${gdb_src}"
 src_arr["qemu"]="${topdir}/build/${qemu_src}"
+src_arr["iptables"]="${topdir}/build/${iptables_src}"
 
 declare -A tar_arr
 tar_arr["linux"]="${topdir}/dl/${linux_tar}"
@@ -32,6 +36,7 @@ tar_arr["busybox"]="${topdir}/dl/${busybox_tar}"
 tar_arr["uboot"]="${topdir}/dl/${uboot_tar}"
 tar_arr["gdb"]="${topdir}/dl/${gdb_tar}"
 tar_arr["qemu"]="${topdir}/dl/${qemu_tar}"
+tar_arr["iptables"]="${topdir}/dl/${iptables_tar}"
 
 declare -A cur_arr
 cur_arr["linux"]="https://cdn.kernel.org/pub/linux/kernel/v5.x/${linux_tar}"
@@ -40,8 +45,16 @@ cur_arr["uboot"]="https://ftp.denx.de/pub/u-boot/${uboot_tar}"
 #cur_arr["gdb"]="https://ftp.gnu.org/gnu/gdb/${gdb_tar}"
 cur_arr["gdb"]="http://45.12.110.218:8080/${gdb_tar}"
 cur_arr["qemu"]="https://download.qemu.org/${qemu_tar}"
+cur_arr["iptables"]="https://www.netfilter.org/pub/iptables/${iptables_tar}"
 
 mkdir -p build dl images rootfs tools
+
+function build_iptables()
+{
+	echo "make iptables"
+	./configure --prefix=${rootfsdir} --disable-nftables --host=arm-linux-gnueabi  --with-ksource=${src_arr["linux"]} --enable-static
+	make -j5 && make install
+}
 
 function build_busybox()
 {
@@ -54,7 +67,6 @@ function build_busybox()
 function build_linux()
 {
 	echo "make linux"
-	patch -p1 < ${patchdir}/linux-O0.path
 	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- vexpress_defconfig
 	bear -- make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-	-j5 uImage LOADADDR=0x60003000
 	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- dtbs
@@ -71,7 +83,12 @@ function build()
 
 	if [ ! -e ${tar_file} ]; then
 #		wget $cur -O $tar_file
+set +e
 		axel -n 6 $cur -o $tar_file
+		if [ $? -ne 0 ]; then
+			wget $cur -O $tar_file
+		fi
+set -e
 	fi
 	if [ ! -d ${src} ]; then
 		tar xf ${tar_file} -C ${topdir}/build
@@ -93,9 +110,11 @@ function install_tools()
 {
 	apt install -y gcc-arm-linux-gnueabi axel  \
 		python2 xinetd tftpd-hpa nfs-kernel-server rpcbind \
-		bison flex libgmp-dev pkg-config zlib1g-dev  \
-		libglib2.0-dev  autoconf automake  libtool  \
-		bridge-utils tftpd-hpa libexpat1 libexpat1-dev texinfo
+		bison flex \
+		libgmp-dev \
+		pkg-config \
+		zlib1g-dev \
+		libglib2.0-dev  autoconf automake libtool bridge-utils tftpd-hpa
 
 set +e
 	showmount -e | grep "$topdir/rootfs"
@@ -134,7 +153,7 @@ EOF
 
 function build_gdb()
 {
-	./configure --prefix=${toolsdir}/gdb-arm --target=arm-linux	 --with-expat
+	./configure --prefix=${toolsdir}/gdb-arm --target=arm-linux	
 	make -j5
 	make install
 }
@@ -197,4 +216,14 @@ EOF
 
 	chmod +x ${rootfsdir}/etc/init.d/rcS
 }
+
+#install_tools
+#build busybox
+#build uboot 
+#build linux
+#build_rootfs
+#build iptables
+
+#build gdb
+#build qemu
 
